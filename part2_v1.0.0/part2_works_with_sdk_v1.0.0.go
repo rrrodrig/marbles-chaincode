888f2b264c87f4067b807bb2b3757882360566e9
Rob <rrrodrig@us.ibm.com>
2016-04-05 17:52:11 -0500

    updated

--- part2_v1.0.0/part2_works_with_sdk_v1.0.0.go
+++ part2_v1.0.0/part2_works_with_sdk_v1.0.0.go
@@ -93,6 +93,7 @@ func (t *SimpleChaincode) init(stub *shim.ChaincodeStub, args []string) ([]byte,
 		return nil, err
 	}
 	
+	var myempty []string
 	var empty []string
 	jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the index
 	err = stub.PutState(marbleIndexStr, jsonAsBytes)
